53fb75cc1a1242b6f4a7df5fbeca6054f67f163c
DevLipatov <yaroslav.lipatov.90@gmail.com>
2019-07-10 23:25:43 +0300

    Add server file

---
+++ server-index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
+app.get('/',
+    (request, response) => {
+        response.json({
+            id: '10',
+            name: 'Moscow'
+        });
+    });
+
+app.get('/hh',
+    (request, response) => {
+        response.json({
+            id: '20',
+            name: 'HH'
+        });
+    });
+
+app.listen(port, (err) => {
+    if (err) {
+        return console.log('something bad happened', err)
+    }
+    console.log(`server is listening on ${port}`)
+});
